469e434a58c19d92f0c8c8a297014206eb9a5cb9
xenoicwyce <tl.bk@hotmail.com>
2020-08-21 22:46:40 +0900

    Initial commit

---
+++ maze.py
@@ -0,0 +1,100 @@
+import networkx as nx
+import json
+import copy
+
+
+DRAW_SOLUTION = True
+
+class MazeNotFound(Exception):
+    pass
+
+def generate_graph(circ):
+    # empty graph instance
+    G = nx.Graph()
+
+    # add nodes to graph
+    for y in range(6):
+        for x in range(6):
+            G.add_node(x+6*y+1, pos=(x+1, -(y+1)))
+
+    # load edge data
+    with open('edge_data.json') as f:
+        edge_data = json.load(f)
+
+    # add edges to graph
+    if circ == '12' or circ == '63':
+        edges = edge_data['1']
+    elif circ == '52' or circ == '24':
+        edges = edge_data['2']
+    elif circ == '64' or circ == '44':
+        edges = edge_data['3']
+    elif circ == '11' or circ == '14':
+        edges = edge_data['4']
+    elif circ == '53' or circ == '46':
+        edges = edge_data['5']
+    elif circ == '51' or circ == '35':
+        edges = edge_data['6']
+    elif circ == '21' or circ == '26':
+        edges = edge_data['7']
+    elif circ == '41' or circ == '34':
+        edges = edge_data['8']
+    elif circ == '32' or circ == '15':
+        edges = edge_data['9']
+    else:
+        raise MazeNotFound('The requested maze is not found. ' \
+                           'Please check the parameters again.')
+
+    G.add_edges_from([tuple(e) for e in edges])
+
+    return G
+
+def get_directions(G, square, triangle):
+    sq_node = int(square[0]) + 6*(int(square[1]) - 1)
+    tri_node = int(triangle[0]) + 6*(int(triangle[1]) - 1)
+
+    shortest_path = nx.shortest_path(G, sq_node, tri_node)
+    pos = nx.get_node_attributes(G, 'pos')
+    shortest_path_coord = [pos[node] for node in shortest_path]
+
+    directions = []
+    for idx in range(len(shortest_path_coord) - 1):
+        current_node = shortest_path_coord[idx]
+        next_node = shortest_path_coord[idx + 1]
+        if next_node[0] > current_node[0]:
+            directions.append('RIGHT')
+        elif next_node[0] < current_node[0]:
+            directions.append('LEFT')
+        elif next_node[1] > current_node[1]:
+            directions.append('UP') # negative numbers
+        elif next_node[1] < current_node[1]:
+            directions.append('DOWN')
+        else:
+            directions = 'Already at the destination. No directions needed.'
+            break
+
+    return shortest_path, directions
+
+
+if __name__ == '__main__':
+    coord = input('Enter coordinates in the form of "xy" for ' \
+                  '"circle, square, triangle" (without quotes): ')
+    coord = [c.strip() for c in coord.split(',')]
+    assert len(coord) == 3, '3 parameters required, {} entered.'.format(len(coord))
+    circ, square, triangle = coord
+
+    G = generate_graph(circ)
+    sp, directions = get_directions(G, square, triangle)
+    print('Directions:', directions)
+
+    # draw solution
+    if DRAW_SOLUTION:
+        G_soln = nx.DiGraph()
+        G_soln.add_nodes_from(G.nodes)
+
+        soln_edges = [(sp[idx], sp[idx+1]) for idx in range(len(sp)-1)]
+        G_soln.add_edges_from(soln_edges)
+
+        pos = nx.get_node_attributes(G, 'pos')
+        nx.draw_networkx_nodes(G_soln, pos)
+        nx.draw_networkx_edges(G_soln, pos, edge_color='r')
+
---
+++ password.py
@@ -0,0 +1,26 @@
+import copy
+
+possible_ans = ['about', 'after', 'again', 'below', 'could',
+                'every', 'first', 'found', 'great', 'house',
+                'large', 'learn', 'never', 'other', 'place',
+                'plant', 'point', 'right', 'small', 'sound',
+                'spell', 'still', 'study', 'their', 'there',
+                'these', 'thing', 'think', 'three', 'water',
+                'where', 'which', 'world', 'would', 'write']
+
+initial_guess = copy.copy(possible_ans)
+for idx in range(5):
+    given = input('Enter all possible character {}: '.format(idx+1))
+    temp = list(filter(lambda word: word[idx] in given, initial_guess))
+    print(temp)
+
+    if len(temp) == 1:
+        print('\nSearch concluded, answer: {}'.format(temp[0]))
+        break
+    elif len(temp) == 0:
+        print('\nSearch concluded, no answer found.')
+        break
+
+    initial_guess = copy.copy(temp)
+
+input('Press ENTER to continue...')
